9941fbf46b706f168b4ccde48318c6e8090b7e2e
Dmytro_Bykanov <dmitbykanov@gmail.com>
2025-11-29 19:48:46 +0200

    upd

--- components/TanstackProvider/TanstackProvider.tsx
+++ components/TanstackProvider/TanstackProvider.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 interface TanStackProviderProps {
   children: React.ReactNode;
 }
+
 export default function TanStackProvider({ children }: TanStackProviderProps) {
   const [queryClient] = useState(() => new QueryClient());
 
